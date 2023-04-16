77d07de1eaabff04c8795a199d38ee7bf29b04eb
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2023-04-16 04:21:11 +0000

    image carousel

--- scripts/listings.js
+++ scripts/listings.js
@@ -1,3 +1,16 @@
+function getCircularReplacer() {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+}
+
 function createMarker(listing) {
   const latlng = fromLocationToLatLng(listing.location, 1, 6);
   const marker = L.marker(latlng);
@@ -6,49 +19,94 @@ function createMarker(listing) {
 }
 
 function handleCardClick(listing) {
+  console.log("Clicked on listing: " + listing.title);
   // Update the sidebar content with the information of the clicked listing
   let sidebarContent = '';
   sidebarContent += '<div class="row">';
-  const card = createCard(listing);
-  sidebarContent += '<div class="col-md-12">' + card.outerHTML + '</div>';
+  sidebarContent += '<div class="col-md-12">';
+  sidebarContent += '<div class="card mb-4 border-light">';
+  
+  // Add carousel items
+  sidebarContent += '<div id="carouselControls" class="carousel slide" data-bs-ride="carousel">';
+  sidebarContent += '<div class="carousel-inner">';
+
+  // Add carousel indicators
+  sidebarContent += '<ol class="carousel-indicators">';
+  for (let i = 0; i < listing.locationImages.length; i++) {
+    sidebarContent += '<li data-bs-target="#carouselControls" data-bs-slide-to="' + i + '"' + (i === 0 ? ' class="active"' : '') + '></li>';
+  }
+  sidebarContent += '</ol>';
+
+  for (let i = 0; i < listing.locationImages.length; i++) {
+    sidebarContent += '<div class="carousel-item' + (i === 0 ? ' active' : '') + '">';
+    sidebarContent += '<img src="' + listing.locationImages[i] + '" class="d-block w-100" draggable="false" alt="...">';
+    sidebarContent += '</div>';
+  }
+
+  // Add carousel left/right controls
   sidebarContent += '</div>';
+  sidebarContent += '<button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">';
+  sidebarContent += '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
+  sidebarContent += '<span class="visually-hidden">Previous</span>';
+  sidebarContent += '</button>';
+  sidebarContent += '<button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">';
+  sidebarContent += '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
+  sidebarContent += '<span class="visually-hidden">Next</span>';
+  sidebarContent += '</button>';
+  sidebarContent += '</div>';
+  sidebarContent += '</div>';
+  sidebarContent += '</div>';
+  sidebarContent += '</div>';
+
+  // Add title, description, and price information
+
+
   sidebar.setContent(sidebarContent);
 }
 
 function createCard(listing) {
+  // Create card
   const card = document.createElement('div');
-  card.classList.add('card', 'mb-4');
+  card.classList.add('card', 'mb-4', 'border-light');
+  card.setAttribute('onclick', 'handleCardClick(' + JSON.stringify(listing, getCircularReplacer()) + ')');
+  card.setAttribute('style', 'cursor: pointer;');
 
+  // Create card image+
   const image = document.createElement('img');
   image.classList.add('card-img-top');
-  image.src = listing.locationImage;
+  image.setAttribute('draggable', 'false');
+  image.src = listing.locationImages[0]
   image.alt = listing.name;
   card.appendChild(image);
 
+  // Create card body
   const body = document.createElement('div');
-  body.classList.add('card-body');
+  body.classList.add('card-body', 'text-light', 'bg-dark', 'rounded-bottom');
   card.appendChild(body);
 
+  // Create card title
   const title = document.createElement('h5');
   title.classList.add('card-title');
   title.textContent = listing.title;
   body.appendChild(title);
 
+  // Create card description
   const description = document.createElement('p');
   description.classList.add('card-text');
   description.textContent = listing.description;
   body.appendChild(description);
 
+  // Create card price
   const price = document.createElement('p');
   price.classList.add('card-text', 'fw-bold');
   price.textContent = '$' + listing.price;
   body.appendChild(price);
 
-  card.addEventListener('click', function() {
-    console.log('Clicked on listing ' + listing.id)
+  // Add click event listener to card
+  card.addEventListener('click', function () {
     handleCardClick(listing);
   });
-
+  
   return card;
 }
 
@@ -71,7 +129,8 @@ function updateListings() {
   // Update the sidebar content with the list of visible listings
   let sidebarContent = '';
   if (visibleListings.length === 0) {
-    sidebarContent = '<h3 style="text-align: center; margin-top: 100%;">No listings found within the current map bounds.</h3>';
+    // Align text to center of sidebar
+    sidebarContent = '<h3 class="text-center">No listings found within the current map bounds.</h3>';
   } else {
     sidebarContent += '<div class="row">';
     for (let i = 0; i < visibleListings.length; i++) {
@@ -93,7 +152,7 @@ var listings = [
     location: { x: 2700, y: 64, z: 4153 },
     marker: null,
     markerImage: null,
-    locationImage: "https://pbs.twimg.com/media/E2zf1ZTWYAcJC4x?format=jpg&name=large"
+    locationImages: ["https://pbs.twimg.com/media/E2zf1ZTWYAcJC4x?format=jpg&name=large"]
   },
   {
     id: 2,
@@ -103,7 +162,7 @@ var listings = [
     location: { x: 2750, y: 64, z: 4153 },
     marker: null,
     markerImage: null,
-    locationImage: "https://i.ytimg.com/vi/QNgYQanbReE/maxresdefault.jpg"
+    locationImages: ["https://i.ytimg.com/vi/QNgYQanbReE/maxresdefault.jpg", "https://topg.org/gallery/370922/64834.png"]
   },
   {
     id: 3,
@@ -113,7 +172,7 @@ var listings = [
     location: { x: 2800, y: 64, z: 4153 },
     marker: null,
     markerImage: null,
-    locationImage: "https://topg.org/gallery/350820/35678.png"
+    locationImages: ["https://topg.org/gallery/350820/35678.png"]
   }
 ];
 
