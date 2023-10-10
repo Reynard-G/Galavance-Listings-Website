310cfbc1362431e95a58a9e750ab017eff07ea3d
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-10-09 23:31:12 -0500

    style: :lipstick: font + correct favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,21 @@
 import '@styles/globals.css';
 import 'leaflet/dist/leaflet.css';
+import { Libre_Franklin } from 'next/font/google';
 
 import React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
-import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+const libreFranklin = Libre_Franklin({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
   return (
     <React.StrictMode>
       <NextUIProvider>
         <NextThemesProvider attribute="class" defaultTheme='dark' enableSystem={false} enableColorScheme={false}>
-          <Component {...pageProps} />
+          <main className={libreFranklin.className}>
+            <Component {...pageProps} />
+          </main>
         </NextThemesProvider>
       </NextUIProvider>
     </React.StrictMode>
