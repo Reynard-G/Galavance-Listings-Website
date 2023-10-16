2774f4ea784d0a57a47615a93777dc8be80ebea2
Reynard-G <reynard.ean.gunawan@gmail.com>
2023-10-16 18:09:01 -0500

    chore: :mute: removed leftover logs

--- pages/admin/listings/edit/[plot].jsx
+++ pages/admin/listings/edit/[plot].jsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 const EditListing = () => {
   const router = useRouter();
   const { plot } = router.query;
-  console.log(plot)
 
   return (
     <p>Edit Listings for {plot}</p>
