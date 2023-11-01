d2c42dc232eaa93ffe817e3bc2757d3da054fed9
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2023-11-01 05:01:24 +0000

    build: :construction_worker: no transformations in dev

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: {
+  plugins: process.env.NODE_ENV === 'production' ? {
     "tailwindcss": {},
     "autoprefixer": {},
     "postcss-flexbugs-fixes": {},
@@ -14,6 +14,6 @@ module.exports = {
         },
         "custom-properties": false,
       },
-    },
-  },
+    }
+  } : {}
 };
