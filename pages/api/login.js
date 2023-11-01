e325246e56d3076265f1fe5723764c98c7403925
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2023-11-01 04:30:52 +0000

    refactor: :fire: removed old mysql connections

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,4 @@
-import { createConnection } from "mariadb";
+import sql from "@lib/db";
 import bcrypt from "bcrypt";
 import { serialize } from "cookie";
 import jwt from "jsonwebtoken";
@@ -14,19 +14,8 @@ export default async function handler(req, res) {
     return res.status(400).json({ message: "Username and password required" });
   }
 
-  const conn = await createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-  });
-
   try {
-    const rows = await conn.query(
-      "SELECT * FROM accounts WHERE username = ?",
-      [username]
-    );
+    const rows = await sql`SELECT * FROM accounts WHERE username = ${username}`
 
     if (rows.length === 0) {
       return res.status(401).json({ message: "Invalid username or password" });
@@ -63,7 +52,5 @@ export default async function handler(req, res) {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
-  } finally {
-    await conn.end();
   }
 }
