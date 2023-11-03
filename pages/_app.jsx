c72518b1bee70a209bfd563951b76e6a4d4e6c8d
Reynard G <40525094+Reynard-G@users.noreply.github.com>
2023-11-03 19:09:57 +0000

    feat: :sparkles: client side routing

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,15 +3,18 @@ import 'leaflet/dist/leaflet.css';
 import { Libre_Franklin } from 'next/font/google';
 
 import React from 'react';
+import { useRouter } from 'next/router';
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const libreFranklin = Libre_Franklin({ subsets: ['latin'], display: 'swap' });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <React.StrictMode>
-      <NextUIProvider>
+      <NextUIProvider navigate={router.push}>
         <NextThemesProvider attribute="class" defaultTheme='dark' enableSystem={false} enableColorScheme={false}>
           <main className={libreFranklin.className}>
             <Component {...pageProps} />
